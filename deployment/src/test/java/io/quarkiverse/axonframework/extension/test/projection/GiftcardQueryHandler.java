9b9978a9a337c32331d52617df48d9bf1d182349
Markus Hager <meks77@gmail.com>
2024-10-15 21:49:16 +0200

    formatted

--- deployment/src/test/java/io/quarkiverse/axonframework/extension/test/projection/GiftcardQueryHandler.java
+++ deployment/src/test/java/io/quarkiverse/axonframework/extension/test/projection/GiftcardQueryHandler.java
@@ -1,6 +1,7 @@
 package io.quarkiverse.axonframework.extension.test.projection;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
