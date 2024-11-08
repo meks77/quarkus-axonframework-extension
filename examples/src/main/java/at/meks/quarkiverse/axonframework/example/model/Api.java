e0099436027f04b11bdf162128c338176704c490
Markus Hager <38517613+meks77@users.noreply.github.com>
2024-11-08 09:29:18 +0100

    Documentation (#56)
    #54 Add Giftcard domain example with Axon Framework
    
    Introduce a comprehensive example using Axon Framework for an Event Sourcing and CQRS application. This includes the domain model for Giftcard, command and event handlers, projections, and integration tests to demonstrate usage. Also, updated the documentation and added dependencies required for the example.

---
+++ examples/src/main/java/at/meks/quarkiverse/axonframework/example/model/Api.java
@@ -0,0 +1,24 @@
+package at.meks.quarkiverse.axonframework.example.model;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+public class Api {
+
+    public record IssueCardCommand(@TargetAggregateIdentifier String id, int initialAmount) {
+    }
+
+    public record CardIssuedEvent(String id, int amount) {
+    }
+
+    public record RedeemCardCommand(@TargetAggregateIdentifier String id, int amount) {
+    }
+
+    public record CardRedeemedEvent(String id, int amount) {
+    }
+
+    public record UndoLatestRedemptionCommand(@TargetAggregateIdentifier String id, int amount) {
+    }
+
+    public record LatestRedemptionUndoneEvent(String id, int amount) {
+    }
+}
---
+++ examples/src/main/java/at/meks/quarkiverse/axonframework/example/model/Giftcard.java
@@ -0,0 +1,88 @@
+package at.meks.quarkiverse.axonframework.example.model;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.axonframework.commandhandling.CommandExecutionException;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.messaging.interceptors.ExceptionHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+
+import io.quarkus.logging.Log;
+
+public class Giftcard {
+
+    @AggregateIdentifier
+    private String id;
+    private int currentAmount;
+    private final List<Integer> cardRedemptions = new ArrayList<>();
+
+    @SuppressWarnings("unused")
+    Giftcard() {
+        // necesarry for the axon framework
+    }
+
+    @CommandHandler
+    Giftcard(Api.IssueCardCommand command) {
+        if (command == null) {
+            throw new IllegalArgumentException("command mustn't be null");
+        }
+        apply(new Api.CardIssuedEvent(command.id(), command.initialAmount()));
+        Log.infof("new card with the id %s and the initial amount %s was issued", command.id(), command.initialAmount());
+    }
+
+    @EventSourcingHandler
+    void handle(Api.CardIssuedEvent event) {
+        this.id = event.id();
+        this.currentAmount = event.amount();
+    }
+
+    @CommandHandler
+    void handle(Api.RedeemCardCommand command) {
+        if (this.currentAmount < command.amount()) {
+            throw new IllegalArgumentException(
+                    "amount(" + command.amount() + ") must be less than current card amount(" + currentAmount + ")");
+        }
+        apply(new Api.CardRedeemedEvent(id, command.amount()));
+        Log.infof("card was redeemed by %s", command.amount());
+    }
+
+    @EventSourcingHandler
+    void handle(Api.CardRedeemedEvent event) {
+        this.currentAmount -= event.amount();
+        cardRedemptions.add(event.amount());
+    }
+
+    public void undoRedemption(int amount) {
+        Optional<Integer> lastestRedeemedAmount = lastestRedeemedAmount();
+        if (lastestRedeemedAmount.isEmpty() || lastestRedeemedAmount.get() != amount) {
+            throw new IllegalArgumentException("amount must be the lastest redeem amount");
+        } else {
+            apply(new Api.LatestRedemptionUndoneEvent(id, amount));
+            Log.infof("latest redemption was undone");
+        }
+    }
+
+    private Optional<Integer> lastestRedeemedAmount() {
+        if (!cardRedemptions.isEmpty()) {
+            return Optional.of(cardRedemptions.get(cardRedemptions.size() - 1));
+        }
+        return Optional.empty();
+    }
+
+    @EventSourcingHandler
+    void handle(Api.LatestRedemptionUndoneEvent event) {
+        cardRedemptions.remove(cardRedemptions.size() - 1);
+        this.currentAmount += event.amount();
+    }
+
+    @ExceptionHandler
+    public void handleAll(Exception exception) {
+        throw new CommandExecutionException("wrapped exception in details", exception);
+    }
+
+}
