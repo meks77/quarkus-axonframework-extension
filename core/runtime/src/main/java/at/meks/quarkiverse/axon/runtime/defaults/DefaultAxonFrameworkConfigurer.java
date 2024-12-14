9a028f1faea5eaaa0bed858432bd19cf0931884c
Markus Hager <meks77@gmail.com>
2024-12-14 20:59:56 +0100

    #30 Refactored interceptor configuration into a dedicated class
    Extracted interceptor registration logic from `DefaultAxonFrameworkConfigurer` to a new `InterceptorConfigurer` class.

--- core/runtime/src/main/java/at/meks/quarkiverse/axon/runtime/defaults/DefaultAxonFrameworkConfigurer.java
+++ core/runtime/src/main/java/at/meks/quarkiverse/axon/runtime/defaults/DefaultAxonFrameworkConfigurer.java
@@ -3,31 +3,18 @@ package at.meks.quarkiverse.axon.runtime.defaults;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 
-import org.axonframework.commandhandling.CommandBus;
-import org.axonframework.commandhandling.CommandExecutionException;
-import org.axonframework.commandhandling.CommandMessage;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.Configurer;
 import org.axonframework.config.DefaultConfigurer;
-import org.axonframework.config.EventProcessingConfigurer;
-import org.axonframework.eventhandling.EventBus;
-import org.axonframework.messaging.InterceptorChain;
-import org.axonframework.messaging.unitofwork.UnitOfWork;
-import org.axonframework.queryhandling.QueryBus;
-import org.axonframework.queryhandling.QueryExecutionException;
-import org.axonframework.queryhandling.QueryMessage;
 import org.axonframework.serialization.json.JacksonSerializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import at.meks.quarkiverse.axon.runtime.conf.AxonConfiguration;
 import at.meks.quarkiverse.axon.runtime.customizations.*;
 import io.quarkus.arc.DefaultBean;
 import io.quarkus.logging.Log;
@@ -58,25 +45,7 @@ class DefaultAxonFrameworkConfigurer implements AxonFrameworkConfigurer {
     Instance<AxonEventProcessingConfigurer> eventProcessingConfigurers;
 
     @Inject
-    Instance<CommandDispatchInterceptorsProducer> commandDispatchInterceptorProducers;
-
-    @Inject
-    Instance<CommandHandlerInterceptorsProducer> commandHandlerInterceptorProducers;
-
-    @Inject
-    Instance<QueryDispatchInterceptorsProducer> queryDispatchInterceptorProducers;
-
-    @Inject
-    Instance<QueryHandlerInterceptorsProducer> queryHandlerInterceptorProducers;
-
-    @Inject
-    Instance<EventDispatchInterceptorsProducer> eventDispatchInterceptorProducers;
-
-    @Inject
-    Instance<EventHandlerInterceptorsProducer> eventHandlerInterceptorProducers;
-
-    @Inject
-    AxonConfiguration axonConfiguration;
+    InterceptorConfigurer interceptorConfigurer;
 
     private Set<Class<?>> aggregateClasses;
     private Set<Object> eventhandlers;
@@ -92,28 +61,28 @@ class DefaultAxonFrameworkConfigurer implements AxonFrameworkConfigurer {
         configurer = DefaultConfigurer.defaultConfiguration()
                 .configureSerializer(conf -> jacksonSerializer)
                 .configureEventSerializer(confg -> jacksonSerializer);
-        eventstoreConfigurer.configure(configurer);
 
+        eventstoreConfigurer.configure(configurer);
+        configureAggregates(configurer);
+        configrueMessageHandler(configurer);
+        configureTransactionManagement(configurer);
+        metricsConfigurer.configure(configurer);
+        interceptorConfigurer.registerInterceptors(configurer);
+        registerInjectableBeans(configurer);
+
+        return configurer;
+    }
+
+    private void configureAggregates(Configurer configurer) {
         aggregateClasses.forEach(
                 aggregate -> configurer.configureAggregate(aggregateConfigurer.createConfigurer(aggregate)));
+    }
 
+    private void configrueMessageHandler(Configurer configurer) {
         configureEventHandling(configurer);
 
         commandhandlers.forEach(handler -> configurer.registerCommandHandler(conf -> handler));
         queryhandlers.forEach(handler -> configurer.registerQueryHandler(conf -> handler));
-
-        configureTransactionManagement(configurer);
-        metricsConfigurer.configure(configurer);
-
-        registerCommandBusInterceptors(configurer);
-        registerQueryBusInterceptors(configurer);
-        registerEventBusInterceptors(configurer);
-
-        for (Map.Entry<Class<?>, Object> entry : injectableBeans.entrySet()) {
-            //noinspection unchecked
-            configurer.registerComponent((Class<Object>) entry.getKey(), configuration -> entry.getValue());
-        }
-        return configurer;
     }
 
     private void configureEventHandling(Configurer configurer) {
@@ -137,144 +106,10 @@ class DefaultAxonFrameworkConfigurer implements AxonFrameworkConfigurer {
         configurer.configureTransactionManager(conf -> transactionManager);
     }
 
-    private void registerCommandBusInterceptors(Configurer configurer) {
-        configurer.onInitialize(configuration -> {
-            CommandBus commandBus = configuration.getComponent(CommandBus.class);
-            configureCommandDispatchInterceptors(commandBus);
-            configureCommandHandlerInterceptors(commandBus);
-        });
-    }
-
-    private void configureCommandDispatchInterceptors(CommandBus bus) {
-        if (commandDispatchInterceptorProducers.isAmbiguous()) {
-            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
-                    CommandDispatchInterceptorsProducer.class.getName(),
-                    toCsv(commandDispatchInterceptorProducers.stream())));
-        } else if (commandDispatchInterceptorProducers.isResolvable()) {
-            commandDispatchInterceptorProducers.get().createDispatchInterceptor()
-                    .forEach(bus::registerDispatchInterceptor);
-        }
-    }
-
-    private <T> String toCsv(Stream<T> stream) {
-        return stream
-                .map(Object::getClass)
-                .map(Class::getName)
-                .collect(Collectors.joining(", "));
-    }
-
-    private void configureCommandHandlerInterceptors(CommandBus bus) {
-        if (axonConfiguration.exceptionHandling().wrapOnCommandHandler()) {
-            //noinspection resource
-            bus.registerHandlerInterceptor(this::handleExceptionInCommandHandling);
-        }
-        if (commandHandlerInterceptorProducers.isAmbiguous()) {
-            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
-                    CommandHandlerInterceptorsProducer.class.getName(),
-                    toCsv(commandHandlerInterceptorProducers.stream())));
-        } else if (commandHandlerInterceptorProducers.isResolvable()) {
-            commandHandlerInterceptorProducers.get().createHandlerInterceptor()
-                    .forEach(bus::registerHandlerInterceptor);
-        }
-    }
-
-    private Object handleExceptionInCommandHandling(UnitOfWork<? extends CommandMessage<?>> unitOfWork,
-            InterceptorChain interceptorChain) {
-        try {
-            return interceptorChain.proceed();
-        } catch (Exception e) {
-            if (!(e instanceof CommandExecutionException)) {
-                throw new CommandExecutionException(
-                        "error while executing command handler for command %s".formatted(
-                                unitOfWork.getMessage().getCommandName()),
-                        e);
-            }
-            throw (CommandExecutionException) e;
-        }
-    }
-
-    private void registerQueryBusInterceptors(Configurer configurer) {
-        configurer.onInitialize(configuration -> {
-            QueryBus queryBus = configuration.queryBus();
-            configureQueryDispatchInterceptors(queryBus);
-            configureQueryHandlerInterceptors(queryBus);
-        });
-    }
-
-    private void configureQueryDispatchInterceptors(QueryBus queryBus) {
-        if (queryDispatchInterceptorProducers.isAmbiguous()) {
-            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
-                    QueryDispatchInterceptorsProducer.class.getName(),
-                    toCsv(queryDispatchInterceptorProducers.stream())));
-        } else if (queryDispatchInterceptorProducers.isResolvable()) {
-            queryDispatchInterceptorProducers.get().createDispatchInterceptor()
-                    .forEach(queryBus::registerDispatchInterceptor);
-        }
-    }
-
-    private void configureQueryHandlerInterceptors(QueryBus queryBus) {
-        configureQueryExceptionInterceptors(queryBus);
-        if (queryHandlerInterceptorProducers.isAmbiguous()) {
-            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
-                    QueryHandlerInterceptorsProducer.class.getName(),
-                    toCsv(queryHandlerInterceptorProducers.stream())));
-        } else if (queryHandlerInterceptorProducers.isResolvable()) {
-            queryHandlerInterceptorProducers.get().createHandlerInterceptor()
-                    .forEach(queryBus::registerHandlerInterceptor);
-        }
-    }
-
-    private void configureQueryExceptionInterceptors(QueryBus queryBus) {
-        if (axonConfiguration.exceptionHandling().wrapOnQueryHandler()) {
-            //noinspection resource
-            queryBus.registerHandlerInterceptor(this::handleExceptionInQueryHandling);
-        }
-    }
-
-    private Object handleExceptionInQueryHandling(UnitOfWork<? extends QueryMessage<?, ?>> unitOfWork,
-            InterceptorChain interceptorChain) {
-        try {
-            return interceptorChain.proceed();
-        } catch (Exception e) {
-            if (!(e instanceof QueryExecutionException)) {
-                throw new QueryExecutionException(
-                        "error while executing query handler for query %s".formatted(
-                                unitOfWork.getMessage().getQueryName()),
-                        e);
-            }
-            throw (QueryExecutionException) e;
-        }
-    }
-
-    private void registerEventBusInterceptors(Configurer configurer) {
-        configurer.onInitialize(configuration -> {
-            EventBus eventBus = configuration.eventBus();
-            configureEventDispatchInterceptors(eventBus);
-        });
-        configureEventHandlerInterceptors(configurer);
-    }
-
-    private void configureEventDispatchInterceptors(EventBus eventBus) {
-        if (eventDispatchInterceptorProducers.isAmbiguous()) {
-            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
-                    EventDispatchInterceptorsProducer.class.getName(),
-                    toCsv(eventDispatchInterceptorProducers.stream())));
-        } else if (eventDispatchInterceptorProducers.isResolvable()) {
-            eventDispatchInterceptorProducers.get().createDispatchInterceptor()
-                    .forEach(eventBus::registerDispatchInterceptor);
-        }
-    }
-
-    private void configureEventHandlerInterceptors(Configurer configurer) {
-        if (eventHandlerInterceptorProducers.isAmbiguous()) {
-            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
-                    EventHandlerInterceptorsProducer.class.getName(),
-                    toCsv(eventHandlerInterceptorProducers.stream())));
-        } else if (eventHandlerInterceptorProducers.isResolvable()) {
-            EventProcessingConfigurer eventProcessingConfigurer = configurer.eventProcessing();
-            eventHandlerInterceptorProducers.get().createHandlerInterceptor()
-                    .forEach(interceptor -> eventProcessingConfigurer
-                            .registerDefaultHandlerInterceptor((configuration, string) -> interceptor));
+    private void registerInjectableBeans(Configurer configurer) {
+        for (Map.Entry<Class<?>, Object> entry : injectableBeans.entrySet()) {
+            //noinspection unchecked
+            configurer.registerComponent((Class<Object>) entry.getKey(), configuration -> entry.getValue());
         }
     }
 
---
+++ core/runtime/src/main/java/at/meks/quarkiverse/axon/runtime/defaults/InterceptorConfigurer.java
@@ -0,0 +1,195 @@
+package at.meks.quarkiverse.axon.runtime.defaults;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.CommandExecutionException;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.config.Configurer;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.messaging.InterceptorChain;
+import org.axonframework.messaging.unitofwork.UnitOfWork;
+import org.axonframework.queryhandling.QueryBus;
+import org.axonframework.queryhandling.QueryExecutionException;
+import org.axonframework.queryhandling.QueryMessage;
+
+import at.meks.quarkiverse.axon.runtime.conf.AxonConfiguration;
+import at.meks.quarkiverse.axon.runtime.customizations.*;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@ApplicationScoped
+class InterceptorConfigurer {
+
+    @Inject
+    Instance<CommandDispatchInterceptorsProducer> commandDispatchInterceptorProducers;
+
+    @Inject
+    Instance<CommandHandlerInterceptorsProducer> commandHandlerInterceptorProducers;
+
+    @Inject
+    Instance<QueryDispatchInterceptorsProducer> queryDispatchInterceptorProducers;
+
+    @Inject
+    Instance<QueryHandlerInterceptorsProducer> queryHandlerInterceptorProducers;
+
+    @Inject
+    Instance<EventDispatchInterceptorsProducer> eventDispatchInterceptorProducers;
+
+    @Inject
+    Instance<EventHandlerInterceptorsProducer> eventHandlerInterceptorProducers;
+
+    @Inject
+    AxonConfiguration axonConfiguration;
+
+    void registerInterceptors(Configurer configurer) {
+        registerCommandBusInterceptors(configurer);
+        registerQueryBusInterceptors(configurer);
+        registerEventBusInterceptors(configurer);
+    }
+
+    private void registerCommandBusInterceptors(Configurer configurer) {
+        configurer.onInitialize(configuration -> {
+            CommandBus commandBus = configuration.getComponent(CommandBus.class);
+            configureCommandDispatchInterceptors(commandBus);
+            configureCommandHandlerInterceptors(commandBus);
+        });
+    }
+
+    private void configureCommandDispatchInterceptors(CommandBus bus) {
+        if (commandDispatchInterceptorProducers.isAmbiguous()) {
+            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
+                    CommandDispatchInterceptorsProducer.class.getName(),
+                    toCsv(commandDispatchInterceptorProducers.stream())));
+        } else if (commandDispatchInterceptorProducers.isResolvable()) {
+            commandDispatchInterceptorProducers.get().createDispatchInterceptor()
+                    .forEach(bus::registerDispatchInterceptor);
+        }
+    }
+
+    private <T> String toCsv(Stream<T> stream) {
+        return stream
+                .map(Object::getClass)
+                .map(Class::getName)
+                .collect(Collectors.joining(", "));
+    }
+
+    private void configureCommandHandlerInterceptors(CommandBus bus) {
+        if (axonConfiguration.exceptionHandling().wrapOnCommandHandler()) {
+            //noinspection resource
+            bus.registerHandlerInterceptor(this::handleExceptionInCommandHandling);
+        }
+        if (commandHandlerInterceptorProducers.isAmbiguous()) {
+            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
+                    CommandHandlerInterceptorsProducer.class.getName(),
+                    toCsv(commandHandlerInterceptorProducers.stream())));
+        } else if (commandHandlerInterceptorProducers.isResolvable()) {
+            commandHandlerInterceptorProducers.get().createHandlerInterceptor()
+                    .forEach(bus::registerHandlerInterceptor);
+        }
+    }
+
+    private Object handleExceptionInCommandHandling(UnitOfWork<? extends CommandMessage<?>> unitOfWork,
+                                                    InterceptorChain interceptorChain) {
+        try {
+            return interceptorChain.proceed();
+        } catch (Exception e) {
+            if (!(e instanceof CommandExecutionException)) {
+                throw new CommandExecutionException(
+                        "error while executing command handler for command %s".formatted(
+                                unitOfWork.getMessage().getCommandName()),
+                        e);
+            }
+            throw (CommandExecutionException) e;
+        }
+    }
+
+    private void registerQueryBusInterceptors(Configurer configurer) {
+        configurer.onInitialize(configuration -> {
+            QueryBus queryBus = configuration.queryBus();
+            configureQueryDispatchInterceptors(queryBus);
+            configureQueryHandlerInterceptors(queryBus);
+        });
+    }
+
+    private void configureQueryDispatchInterceptors(QueryBus queryBus) {
+        if (queryDispatchInterceptorProducers.isAmbiguous()) {
+            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
+                    QueryDispatchInterceptorsProducer.class.getName(),
+                    toCsv(queryDispatchInterceptorProducers.stream())));
+        } else if (queryDispatchInterceptorProducers.isResolvable()) {
+            queryDispatchInterceptorProducers.get().createDispatchInterceptor()
+                    .forEach(queryBus::registerDispatchInterceptor);
+        }
+    }
+
+    private void configureQueryHandlerInterceptors(QueryBus queryBus) {
+        configureQueryExceptionInterceptors(queryBus);
+        if (queryHandlerInterceptorProducers.isAmbiguous()) {
+            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
+                    QueryHandlerInterceptorsProducer.class.getName(),
+                    toCsv(queryHandlerInterceptorProducers.stream())));
+        } else if (queryHandlerInterceptorProducers.isResolvable()) {
+            queryHandlerInterceptorProducers.get().createHandlerInterceptor()
+                    .forEach(queryBus::registerHandlerInterceptor);
+        }
+    }
+
+    private void configureQueryExceptionInterceptors(QueryBus queryBus) {
+        if (axonConfiguration.exceptionHandling().wrapOnQueryHandler()) {
+            //noinspection resource
+            queryBus.registerHandlerInterceptor(this::handleExceptionInQueryHandling);
+        }
+    }
+
+    private Object handleExceptionInQueryHandling(UnitOfWork<? extends QueryMessage<?, ?>> unitOfWork,
+                                                  InterceptorChain interceptorChain) {
+        try {
+            return interceptorChain.proceed();
+        } catch (Exception e) {
+            if (!(e instanceof QueryExecutionException)) {
+                throw new QueryExecutionException(
+                        "error while executing query handler for query %s".formatted(
+                                unitOfWork.getMessage().getQueryName()),
+                        e);
+            }
+            throw (QueryExecutionException) e;
+        }
+    }
+
+    private void registerEventBusInterceptors(Configurer configurer) {
+        configurer.onInitialize(configuration -> {
+            EventBus eventBus = configuration.eventBus();
+            configureEventDispatchInterceptors(eventBus);
+        });
+        configureEventHandlerInterceptors(configurer);
+    }
+
+    private void configureEventDispatchInterceptors(EventBus eventBus) {
+        if (eventDispatchInterceptorProducers.isAmbiguous()) {
+            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
+                    EventDispatchInterceptorsProducer.class.getName(),
+                    toCsv(eventDispatchInterceptorProducers.stream())));
+        } else if (eventDispatchInterceptorProducers.isResolvable()) {
+            eventDispatchInterceptorProducers.get().createDispatchInterceptor()
+                    .forEach(eventBus::registerDispatchInterceptor);
+        }
+    }
+
+    private void configureEventHandlerInterceptors(Configurer configurer) {
+        if (eventHandlerInterceptorProducers.isAmbiguous()) {
+            throw new IllegalStateException("multiple implementations of %s found: %s".formatted(
+                    EventHandlerInterceptorsProducer.class.getName(),
+                    toCsv(eventHandlerInterceptorProducers.stream())));
+        } else if (eventHandlerInterceptorProducers.isResolvable()) {
+            EventProcessingConfigurer eventProcessingConfigurer = configurer.eventProcessing();
+            eventHandlerInterceptorProducers.get().createHandlerInterceptor()
+                    .forEach(interceptor -> eventProcessingConfigurer
+                            .registerDefaultHandlerInterceptor((configuration, string) -> interceptor));
+        }
+    }
+}
