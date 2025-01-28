4b58bd9a76f7bc7caa62422ba271b0f687070dca
Markus Hager <38517613+meks77@users.noreply.github.com>
2025-01-28 08:20:53 +0100

    #50: Extend Dev UI for saga event handlers (#132)
    Added support for saga event handlers in Dev UI, including new table data pages and build-time data handling.
    
    Extracted browser interaction logic into `DevUiBrowser` and assertions into `UiAsserter` to improve test structure and readability.

---
+++ core/deployment/src/test/java/at/meks/quarkiverse/axon/deployment/devui/DevUiBrowser.java
@@ -0,0 +1,62 @@
+package at.meks.quarkiverse.axon.deployment.devui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.microsoft.playwright.*;
+
+public class DevUiBrowser implements AutoCloseable {
+
+    public static final int FIRST_TABLE_CONTENT_ROW = 3;
+
+    private final Playwright playwright;
+    private final Browser browser;
+    private Page devUiPage;
+
+    public DevUiBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.firefox().launch();
+        newPage();
+    }
+
+    private void newPage() {
+        devUiPage = browser.newPage();
+        devUiPage.navigate("http://localhost:8081/q/dev-ui/extensions");
+    }
+
+    Locator lineLocatorInExtensionCard(String lineTitle) {
+        return devUiPage.locator("css=qwc-extension-link[displayname=\"" + lineTitle + "\"]");
+    }
+
+    Locator countLocatorOfLine(Locator lineLocator) {
+        return lineLocator.locator("css= qui-badge > span:not([theme])");
+    }
+
+    Locator titleLocatorOfLine(Locator lineLocator) {
+        return lineLocator.locator("css= > a > span");
+    }
+
+    List<String> getSingleColumnTableAfterClickOnLine(String lineTitle) {
+        lineLocatorInExtensionCard(lineTitle).locator("css= > a").click();
+        return getAsSingleColumnTable(devUiPage.locator("css=vaadin-grid"));
+    }
+
+    private List<String> getAsSingleColumnTable(Locator tableSelector) {
+        List<ElementHandle> rows = tableSelector.locator("css=> vaadin-grid-cell-content").elementHandles();
+        List<String> table = new ArrayList<>(rows.size());
+
+        for (int i = FIRST_TABLE_CONTENT_ROW; i < rows.size(); i++) {
+            ElementHandle row = rows.get(i);
+            table.add(row.textContent());
+        }
+
+        return table;
+    }
+
+    @Override
+    public void close() {
+        if (playwright != null) {
+            playwright.close();
+        }
+    }
+}
