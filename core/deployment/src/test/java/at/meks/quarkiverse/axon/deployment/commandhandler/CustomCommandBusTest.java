d2ad6bbace750556f4efba14c76c47d8897dabb2
Markus Hager <markus.hager@itsv.at>
2026-06-16 15:24:28 +0200

    WIP: upgrade axon framework to 5.*
    * upgraded transaction using QuarkusTransaction
    * modified test CustomCommandBusTest to be able to test this use case

--- core/deployment/src/test/java/at/meks/quarkiverse/axon/deployment/commandhandler/CustomCommandBusTest.java
+++ core/deployment/src/test/java/at/meks/quarkiverse/axon/deployment/commandhandler/CustomCommandBusTest.java
@@ -1,29 +1,55 @@
 package at.meks.quarkiverse.axon.deployment.commandhandler;
 
-import org.junit.jupiter.api.Disabled;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.axonframework.common.configuration.Configuration;
+import org.axonframework.messaging.commandhandling.CommandBus;
+import org.axonframework.messaging.commandhandling.SimpleCommandBus;
+import org.axonframework.messaging.core.unitofwork.ProcessingContext;
+import org.axonframework.messaging.core.unitofwork.UnitOfWorkFactory;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import at.meks.quarkiverse.axon.runtime.customizations.CommandBusProducer;
 import at.meks.quarkiverse.axon.shared.unittest.JavaArchiveTest;
+import io.quarkus.test.QuarkusExtensionTest;
 
 @Disabled("kein AsynchronousCommandBus?")
 public class CustomCommandBusTest extends JavaArchiveTest {
 
-    //    @RegisterExtension
-    //    static final QuarkusUnitTest config = application(javaArchiveBase()
-    //            .addClasses(MyCommandBusProducer.class));
+        @RegisterExtension
+        static final QuarkusExtensionTest config = application(javaArchiveBase()
+                .addClasses(MyCommandBusProducer.class));
 
-    //    @ApplicationScoped
-    //    public static class MyCommandBusProducer implements CommandBusProducer {
-    //
-    //        @Override
-    //        public CommandBus createCommandBus(Configuration configuration) {
-    //            return AsynchronousCommandBus.builder().build();
-    //        }
-    //    }
-    //
-    //    @Override
-    //    protected void assertConfiguration(Configuration configuration) {
-    //        CommandBus commandBus = configuration.commandBus();
-    //        assertThat(commandBus).isInstanceOf(AsynchronousCommandBus.class);
-    //    }
+        @ApplicationScoped
+        public static class MyCommandBusProducer implements CommandBusProducer {
+
+            @Override
+            public CommandBus createCommandBus(Configuration configuration) {
+                return new MyCommandBus(configuration.getComponent(UnitOfWorkFactory.class));
+            }
+        }
+
+        private static class MyCommandBus extends SimpleCommandBus {
+
+            /**
+             * Construct a {@code SimpleCommandBus}, using the given {@code unitOfWorkFactory} to construct
+             * {@link ProcessingContext contexts} to handle commands in.
+             *
+             * @param unitOfWorkFactory the {@code UnitOfWorkFactory} used to construct {@link ProcessingContext contexts} to
+             *                          handle commands in
+             */
+            public MyCommandBus(UnitOfWorkFactory unitOfWorkFactory) {
+                super(unitOfWorkFactory);
+            }
+        }
+
+        @Override
+        protected void assertConfiguration(Configuration configuration) {
+            CommandBus commandBus = configuration.getComponent(CommandBus.class);
+            assertThat(commandBus).isInstanceOf(MyCommandBus.class);
+        }
 
 }
