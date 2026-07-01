df4fdd6f1a6ebe76af10fd53570e4f83c75d65ef
Christian Kühmayer <christian.kuehmayer@itsv.at>
2026-07-01 10:13:57 +0200

    WIP snapshotting
    fixed test, snapshot keys include a nul byte that we were not aware of, including this makes the test work.
    
    we still need to find a proper way to configure snapshotting via extension that is not implicit

--- integration-tests/integration-tests-axon-server/src/main/java/at/meks/quarkiverse/axon/it/SystemResource.java
+++ integration-tests/integration-tests-axon-server/src/main/java/at/meks/quarkiverse/axon/it/SystemResource.java
@@ -34,7 +34,21 @@ public class SystemResource {
         CompletableFuture<GetLastSnapshotResponse> lastSnapshotFuture = axonConnectionManager.getConnection(
                         axonContext).snapshotChannel()
                 .getLastSnapshot(GetLastSnapshotRequest.newBuilder()
-                        .setKey(ByteString.copyFromUtf8("giftcard.Giftcard" + aggregateId)).build());
+                        .setKey(buildKey(aggregateId)).build());
         return lastSnapshotFuture.get().hasSnapshot() ? 1 : 0;
+    }
+
+    /**
+     * The key is built by either FQN or annotated nqmespace + name of the entity,
+     * followed by a nul byte, followed by the aggregate ID.
+     * Our test has namespace/name configured and we just us it hardcoded in the API
+     *
+     * @param aggregateId the aggregate ID
+     * @return byte string key of the snapshot
+     */
+    private static ByteString buildKey(String aggregateId) {
+        return ByteString.copyFromUtf8("giftcard.Giftcard").concat(
+                ByteString.copyFrom(new byte[]{0})).concat(
+                ByteString.copyFromUtf8(aggregateId));
     }
 }
